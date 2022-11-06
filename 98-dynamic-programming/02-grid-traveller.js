7d3e5cff4e759e90be91cb6ff8c94124351c83c7
mehtapratik <mehta.pratik@live.com>
2022-11-06 17:20:42 -0500

    grid traveller

--- 98-dynamic-programming/02-grid-traveller.js
+++ 98-dynamic-programming/02-grid-traveller.js
@@ -9,24 +9,31 @@ to reach the bottom-right corner.
 
 /*
 Without Memoization: O(2^(r+c))T and O(r+c)S
-With Memoization: O(r+c)TS
+With Memoization:O(r*c)TS
 */
 function gridTraveller(r, c, cache = {}) {
+   // if 2x3 matrix has 3 possible path then 3x2 matrix will also
+   // have 3 possible paths, therefore, cache key of row_col can be
+   // laveraged for col_row as well.
+   const row_col = `${r},${c}`;
+   const col_row = `${c},${r}`;
+   if (row_col in cache) {
+      return cache[row_col];
+   }
+   if (col_row in cache) {
+      return cache[col_row];
+   }
+
    if (r === 0 || c === 0) {
       return 0;
    }
    if (r === 1 || c === 1) {
       return 1;
    }
-   const cache_key = `${r},${c}`;
-   if (cache_key in cache) {
-      return cache[cache_key];
-   }
 
-   cache[cache_key] =
+   cache[row_col] =
       gridTraveller(r - 1, c, cache) + gridTraveller(r, c - 1, cache);
-
-   return cache[cache_key];
+   return cache[row_col];
 }
 
 console.log(gridTraveller(8, 8));
