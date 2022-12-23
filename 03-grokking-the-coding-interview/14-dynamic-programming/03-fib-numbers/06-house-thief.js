0c280bb4f2adfa96a681b159c13f219d56bf60bf
mehtapratik <mehta.pratik@live.com>
2022-12-23 10:22:03 -0500

    helpful documentation and comments

--- 03-grokking-the-coding-interview/14-dynamic-programming/03-fib-numbers/06-house-thief.js
+++ 03-grokking-the-coding-interview/14-dynamic-programming/03-fib-numbers/06-house-thief.js
@@ -1,28 +1,25 @@
 //
 // INSTRUCTIONS
 //
+// There are n houses built in a line. A thief wants to steal the maximum possible
+// money from these houses.The only restriction the thief has is that he can’t steal
+// from two consecutive houses, as that would alert the security system.
+// How should the thief maximize his stealing?
+//
+// Given a number array representing the wealth of n houses, determine the maximum
+// amount of money the thief can steal without alerting the security system.
+//
 
 //
 // EXAMPLE
 //
-
+// Input: {2, 5, 1, 3, 6, 2, 4}
+// Output: 15
+// Explanation: The thief should steal from houses 5 + 6 + 4
 //
-// EXPLANATION
-//
-// positive values only?
-// zero value considered?
-// non numeric value possibility?
-//
-// [] = 0
-// [1, 1, 1, 1, 1]
-// [1, 2, 3, 4, 5]
-// [5, 4, 3, 2, 1]
-// *
-// 2, 5, 1, 3, 6
-//
-// Maximum money received from these two options
-// option 1: give up money and go to next house (+1)
-// option 2: take money and skip a house (+2)
+// Input: {2, 10, 14, 8, 1}
+// Output: 18
+// Explanation: The thief should steal from houses 10 + 8
 //
 
 //
