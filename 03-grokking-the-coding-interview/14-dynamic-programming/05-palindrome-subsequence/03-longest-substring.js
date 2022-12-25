229acf1213d478b3580b520dea90d740124fdd21
mehtapratik <mehta.pratik@live.com>
2022-12-24 19:14:17 -0500

    dynamic programming | added missing tabulation approach

--- 03-grokking-the-coding-interview/14-dynamic-programming/05-palindrome-subsequence/03-longest-substring.js
+++ 03-grokking-the-coding-interview/14-dynamic-programming/05-palindrome-subsequence/03-longest-substring.js
@@ -31,7 +31,8 @@ function longestPalindromicLength(input) {
 
    const cache = {};
    // return recursive(0, input.length - 1);
-   return memoization(0, input.length - 1);
+   // return memoization(0, input.length - 1);
+   return tabulation();
 
    // O(3^n)T | O(n)S
    // worst case being 'c4b2a1a3b5c' - we're alternating beween same character between both end and
@@ -111,6 +112,51 @@ function longestPalindromicLength(input) {
 
       return true;
    }
+
+   // O(n^2)TS
+   function tabulation() {
+      const table = Array(input.length)
+         .fill(0)
+         .map(() => Array(input.length).fill(0));
+
+      // at the very least, every character is its own palindrome
+      for (let i = 0; i < input.length; i++) {
+         table[i][i] = 1;
+      }
+
+      // rows of matrix -> start with 0th char until end of the string
+      for (let start = 0; start < input.length; start++) {
+         // cols of matrix -> go reverse from previous character of start
+         // position to first character of the string
+         for (let end = start - 1; end >= 0; end--) {
+            const longestPalindromeAtLeft = table[start - 1][end];
+            const longestPalindromeAtRight = table[start][end + 1];
+            const innerPalindromeLength = table[start - 1][end + 1];
+            // every character is at least its own palindrome
+            let currentPalindromeLength = 1;
+
+            // start and end character position will never be same cause inner loop is reverse from `start - 1` to 0
+            if (input[start] === input[end]) {
+               if (
+                  // current palindrome is two character long (e.g. `start=b` and `end=b` in `abbcdef`)
+                  start - end === 1 ||
+                  // inner string between start and end character is palindrome too
+                  // (e.g. `start=a` and`end=a` in `abcba`)
+                  innerPalindromeLength === start - end - 1
+               ) {
+                  currentPalindromeLength = 2 + innerPalindromeLength;
+               }
+            }
+
+            table[start][end] = Math.max(
+               currentPalindromeLength,
+               longestPalindromeAtLeft,
+               longestPalindromeAtRight
+            );
+         }
+      }
+      return table[input.length - 1][0];
+   }
 }
 
 //
@@ -120,3 +166,7 @@ console.log(longestPalindromicLength("abdbca"));
 console.log(longestPalindromicLength("nassan"));
 console.log(longestPalindromicLength("pqrs"));
 console.log(longestPalindromicLength("c4b2a1a3b5c"));
+console.log(longestPalindromicLength("abbbca"));
+console.log(longestPalindromicLength("abac"));
+console.log(longestPalindromicLength("aabcd"));
+console.log(longestPalindromicLength("acbbba"));
