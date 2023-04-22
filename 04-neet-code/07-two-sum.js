1f410ca02927039587e82a88c26451c3a7b7c8fc
mehtapratik <mehta.pratik@live.com>
2023-04-21 22:05:43 -0400

    neetcode problems

---
+++ 04-neet-code/07-two-sum.js
@@ -0,0 +1,54 @@
+//
+// INSTRUCTIONS
+//
+// Given an array of integers nums and an integer target, return indices of the two numbers such
+// that they add up to target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same
+// element twice.
+//
+// You can return the answer in any order.
+//
+
+//
+// EXAMPLE
+//
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+//
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+//
+
+//
+// CODE
+//
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+   const remainders = {};
+   for (let i = 0; i < numbers.length; i++) {
+      const current = numbers[i];
+      if (current in remainders) {
+         return [remainders[current], i];
+      } else {
+         remainders[target - current] = i;
+      }
+   }
+
+   return [];
+};
+
+//
+// TEST
+//
